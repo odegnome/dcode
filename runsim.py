0b3b309165b36c6d426f745b9741dceb94bdb34d
Coder-RG <36698583+Coder-RG@users.noreply.github.com>
2022-03-21 23:58:33 +0530

    Way too many changes

--- runsim.py
+++ runsim.py
@@ -1,14 +1,7 @@
 from dm_control import mujoco
 from PIL import Image
 import os
-from woComposer import QuadEnv
-
-def sim_dir_cleanup():
-    os.system('python3 clearsimulation.py')
-    if not os.access('simulation',os.F_OK):
-        os.mkdir('simulation')
-
-physics = mujoco.Physics.from_xml_path('quad.xml')
+from training import QuadEnv
 
 FRAMERATE = 30
 REPLAY_MEMORY_SIZE = 2400
@@ -16,8 +9,14 @@ MAX_EPISODES = 1000
 MAX_EPISODE_LEN = 24000 # 60(sec)/0.005(timestep)*10(12,000 otherwise)
 replay_memory = []
 
+env = QuadEnv('quad.xml')
+
+def sim_dir_cleanup():
+    os.system('python3 clearsimulation.py')
+    if not os.access('simulation',os.F_OK):
+        os.mkdir('simulation')
+
 def simulate():
-    flie = open('logs.txt','w')
     count = 0
     physics.reset()
     while round(physics.data.time,3) < duration:
@@ -31,19 +30,7 @@ def simulate():
             im = Image.fromarray(pixels)
             im.save(f"simulation/frame{count:03}.jpeg")
             count += 1
-    flie.close()
     return detect_contact(physics)
-
-def detect_contact(physics):
-    print(physics.data.time)
-    print(physics.data.ncon)
-    for i_con in range(physics.data.ncon):
-        id_geom1 = physics.data.contact[i_con].geom1
-        id_geom2 = physics.data.contact[i_con].geom2
-        name_geom1 = physics.model.id2name(id_geom1,'geom')
-        name_geom2 = physics.model.id2name(id_geom2,'geom')
-        print(name_geom1, name_geom2)
-    return 0
 
 def main():
     env = QuadEnv()
--- test_dm_control.py
+++ test_dm_control.py
@@ -1,5 +1,7 @@
 from dm_control import mujoco
 from dm_control import viewer
+import numpy as np
+from PIL import Image
 
 # Load a model from an MJCF XML path.
 physics = mujoco.Physics.from_xml_path('quad.xml')
@@ -7,6 +9,26 @@ physics = mujoco.Physics.from_xml_path('quad.xml')
 physics.step()
 
 # Print the orientation of the geoms.
-print(physics.named.data.xquat)
-print(physics.data.xquat[6])
-print(physics.named.data.xquat['quad'])
+# print(physics.named.data.xquat)
+# print(physics.data.xquat[6])
+# print(physics.named.data.xquat['quad'])
+# print(physics.data.actuator_force)
+
+a = np.concatenate((physics.data.sensordata, physics.data.xquat[6], physics.data.qvel, physics.data.actuator_force))
+# print(a.shape) # (34,)
+
+physics.set_control([0.22,0.22,0.22,0.22])
+for _ in range(800):
+    physics.step()
+
+pixels = physics.render(height=480,width=640,camera_id='fixed_camera')
+Image.fromarray(pixels).save('before_reset.png')
+
+physics.reset()
+pixels = physics.render(height=480,width=640,camera_id='fixed_camera')
+Image.fromarray(pixels).save('after_reset.png')
+
+# print("Sensor data", len(physics.data.sensordata))
+# print("Orientaton: ", len(physics.data.xquat[6]))
+# print("Lin/Ang Velocity: ", len(physics.data.qvel))
+# print("Rotor control: ", len(physics.data.actuator_force))
